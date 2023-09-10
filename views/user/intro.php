73df759eab4b82f05af8e2fa0a109b5c4e4961b0
mohamed693 <mohamed.nasr20977@gmail.com>
2023-09-10 08:58:15 +0300

    add intro section for user

---
+++ views/user/intro.php
@@ -0,0 +1,13 @@
+    <!-- start intro section -->
+    <section class="container text-white-50 mt-2 mb-3">
+        <h1 class="text-white mt-4">Welcome</h1>
+        <p class="mt-4">welcome to my website for PHP 8.* Notes and <a href="https://www.youtube.com/@ProgramWithGio/playlists">Gio
+                Course</a>.</p>
+        <p class="mt-4">You can Contribute on the website and share Notes, Visit <a href="">Contribute page</a> to know
+            more.
+        </p>
+        <p class="mt-4"><span class="fw-bold">NOTE::</span> I would prefere to watch the Course in pararell with website
+            to understad Easily.</p>
+        <p class="fw-bold mt-4">Good Luck!!</p>
+    </section>
+    <!-- end intro section  -->
