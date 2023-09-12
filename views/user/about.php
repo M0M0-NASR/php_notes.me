815d528346dde2cb7b155412bbe7d5fb34870eb6
mohamed693 <mohamed.nasr20977@gmail.com>
2023-09-12 17:05:10 +0300

    create about View

---
+++ views/user/about.php
@@ -0,0 +1,7 @@
+<?php Core\View::load("inc/header"); ?>
+<?php Core\View::load("user/navbar", ["acitve" => "acitve"]); ?>
+<div class="container text-white text-center mt-3 mb-3">
+    <p class="fw-bold fs-1">Not Build Yet!!.</p>
+</div>
+
+<?php Core\View::load("inc/footer"); ?>
