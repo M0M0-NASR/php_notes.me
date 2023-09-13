2926018e63ea565a23fcd23723951dfbece19f1d
mohamed693 <mohamed.nasr20977@gmail.com>
2023-09-13 19:40:30 +0300

    create Error view

---
+++ views/inc/error.php
@@ -0,0 +1,10 @@
+<?php Core\View::load("inc/header"); ?>
+
+<div class="container mt-3 mb-3 text-white">
+
+    <p class="fw-bold fs-1 text-center">500 server Error</p>
+
+    <p><?php echo $msg ?></p>
+
+</div>
+<?php Core\View::load("inc/footer"); ?>
